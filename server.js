e3a05f40c59ad7685c6810d184d9aa0c5ff04c1c
NiniS-prog <129887165+NiniS-prog@users.noreply.github.com>
2024-04-29 21:14:48 +0300

    Initial commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    debug: true // Enable debugging
+    requireTLS: true // Use STARTTLS for encryption
 });
 
 
